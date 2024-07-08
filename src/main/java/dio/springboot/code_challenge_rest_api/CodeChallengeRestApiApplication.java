abd3d1aaed1c73185b3652c8a23b05d775f72b22
rodrigocandidodev <rodrigo_candido.contato@outlook.com>
2024-07-08 16:51:44 -0300

    initial commit

---
+++ src/main/java/dio/springboot/code_challenge_rest_api/CodeChallengeRestApiApplication.java
@@ -0,0 +1,13 @@
+package dio.springboot.code_challenge_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallengeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChallengeRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/springboot/code_challenge_rest_api/CodeChallengeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springboot.code_challenge_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallengeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
