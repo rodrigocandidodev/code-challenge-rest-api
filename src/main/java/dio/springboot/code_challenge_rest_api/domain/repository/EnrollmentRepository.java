da0d6ee4f3203b914349fed1ba6d70960d8dd59d
rodrigocandidodev <rodrigo_candido.contato@outlook.com>
2024-07-08 17:12:02 -0300

    feat: add enrollment repository

---
+++ src/main/java/dio/springboot/code_challenge_rest_api/domain/repository/EnrollmentRepository.java
@@ -0,0 +1,6 @@
+package dio.springboot.code_challenge_rest_api.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnrollmentRepository extends JpaRepository {
+}
